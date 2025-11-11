0f4363d73b97acc747416054d38d8e44dc4302f8
NasifaThahseenKM <nasifathahseenkm@gmail.com>
2025-11-11 12:10:10 +0530

    MySite-CW1

--- todoproject/greeting/views.py
+++ todoproject/greeting/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+def greeting(request):
+    return render(request,'index.html')
 
-# Create your views here.
+def greeting(request):
+    return render(request, 'index2.html')
