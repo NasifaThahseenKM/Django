8b948e56ad0a11ffb717a8309e0947ec1b9584c3
NasifaThahseenKM <nasifathahseenkm@gmail.com>
2025-11-13 00:44:59 +0530

    MyWebPage - CW2

--- mywebpage/basicpages/static/script.js
+++ mywebpage/basicpages/static/script.js
@@ -1,3 +1,8 @@
-function showMessage() {
-    alert("Button clicked! Welcome!");
-}
+document.addEventListener("DOMContentLoaded", function() {
+    const button = document.getElementById("greet-btn");
+    if (button) {
+        button.addEventListener("click", function() {
+            alert("Hello! Thanks for visiting my page!");
+        });
+    }
+});
